ac96525f4570ebb634cb7afc2a3f5852c25b66b3
Liam <liam.folsviks@gmail.com>
2026-04-25 12:58:48 +0200

    test_code

--- usv_logic/WaypointGeneratorNode.py
+++ usv_logic/WaypointGeneratorNode.py
@@ -1,35 +1,75 @@
 import numpy as np
 import rclpy
 from rclpy.node import Node
-
-from waypoint_msgs.msg import WaypointPass
+from waypoint_msgs.msg import Waypoint
+from waypoint_msgs.msg import Waypoints
 
 
 class WaypointGeneratorNode(Node):
     def __init__(self):
         super().__init__("usv_logic")
         self.publisher_ = self.create_publisher(
-            WaypointPass, "selene/waypoint/pass", 10
+            Waypoints, "selene/controller/waypoints", 10
         )
-        timer_period = 0.
-        self.timer = self.create_timer(timer_period, self.waypoint_pass_cb)
+        self.single_waypoint_publisher_ = self.create_publisher(
+            Waypoint, "selene/controller/waypoint", 10
+        )
+        #timer_period = 0.0
+        #self.timer = self.create_timer(timer_period, self.waypoint_send_cb)
         self.get_logger().info("Waypoint generator started")
+        self.waypoint_send_cb()
 
-    def waypoint_pass_cb(self):
-        radius = 3.5
-        angles = np.linspace(0.0, 2*3.14, num=100, dtype=float)
+        #waypoint = Waypoint()
+        #waypoint.x = 8.1
+        #waypoint.y = 8.25
+        #waypoint.radius = 0.1;
+        #waypoint.type = Waypoint.HOLD
+        #waypoint.keep_on_track = True
+        #waypoint.hold = True
+        #self.single_waypoint_publisher_.publish(waypoint)
+        #print("Waypoint published")
+
+    def waypoint_send_cb(self):
+        x,y = self.generate_infinity_symbol(
+            x_center=0.0,
+            y_center=0.0,
+            x_distance=10.0,
+            y_distance=10.0
+        )
+
+        waypoint_list = Waypoints()
+        for i in range(np.size(x)):
+            waypoint = Waypoint()
+            waypoint.type = Waypoint.PASS
+            waypoint.x = x[i]
+            waypoint.y = y[i]
+            waypoint.keep_on_track = True
+            waypoint.radius = 0.1
+            waypoint_list.waypoints.append(waypoint)
+        
+        self.publisher_.publish(waypoint_list)
+        print("Waypoints are published")
+
+
+    def generate_infinity_symbol(self,*,x_center,y_center,x_distance, y_distance):
+        # https://math.stackexchange.com/questions/1277790/is-there-a-closed-form-expression-for-the-infinity-symbol
+
+        angles = np.linspace(0.0, 2 * 3.14, num=100, dtype=float)
+        x_list = []
+        y_list = []
+
         for angle in angles:
-            wp_pass = WaypointPass()
-            wp_pass.waypoint.x = radius * np.cos(angle)
-            wp_pass.waypoint.y = radius * np.sin(angle)
-            wp_pass.waypoint.radius = 0.2
+            x_list.append(x_center + x_distance / 2.0 * np.sin(2.0*angle))
+            y_list.append(y_center + y_distance / 2.0 * np.sin(angle))
+        return np.array(x_list), np.array(y_list)
 
-            self.publisher_.publish(wp_pass)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
     waypoint_generator = WaypointGeneratorNode()
-    rclpy.spin(waypoint_generator)
+    rclpy.spin_once(waypoint_generator)
     rclpy.shutdown()
 
 
